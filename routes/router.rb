0a9ffc1f8619915e6ff1c9e66b4ba8fb24f9f33d
Artem Novikov <artemnovikov@Artems-MacBook-Pro.local>
2023-07-03 12:49:33 +0300

    router#match refactoring

--- routes/router.rb
+++ routes/router.rb
@@ -12,25 +12,13 @@ class Router
 
   def match(http_method, path, &handler)
     segments = path.split('/').reject(&:empty?)
-    params_keys = segments.select { |seg| seg.start_with?(':') || seg.start_with?('*') }.map { |seg| seg[1..] }
-
-    segments.map! do |seg|
-      if seg.start_with?(':')
-        '(\w+)'
-      elsif seg.start_with?('*')
-        '(.+)'
-      else
-        seg
-      end
-    end
-
-    path = "/#{segments.join('/')}"
+    params_keys = extract_params_keys(segments)
+    path = build_path_regex(segments)
 
     route = Route.new(http_method, path, handler, params_keys)
 
     @routes << route
 
-    # Debug output
     debug_output("Added route: method=#{http_method}, path=#{path}, block=#{handler}, params_keys=#{params_keys}")
   end
 
@@ -47,5 +35,25 @@ class Router
     end
 
     [nil, {}]
+  end
+
+  private
+
+  def extract_params_keys(segments)
+    segments.select { |seg| seg.start_with?(':') || seg.start_with?('*') }.map { |seg| seg[1..] }
+  end
+
+  def build_path_regex(segments)
+    segments.map! do |seg|
+      if seg.start_with?(':')
+        '(\w+)'
+      elsif seg.start_with?('*')
+        '(.+)'
+      else
+        seg
+      end
+    end
+
+    "/#{segments.join('/')}"
   end
 end
